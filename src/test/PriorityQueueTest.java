db3ca738bf0216bbb3c69d11af64a7e6f1fd3740
0xbe61a55f <0xbe61a55f@gmail.com>
2022-03-17 12:00:51 +0800

    fix

--- src/test/PriorityQueueTest.java
+++ src/test/PriorityQueueTest.java
@@ -1,22 +1,22 @@
-import java.util.PriorityQueue;
-import java.util.stream.Stream;
-import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
 
 
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider(){
-
+        //  return stream
         return Stream.of(
-                arguments(new int[]{3,8,2,4},new int[] {3,4,2,8}),
-                arguments(new int[]{2,4,6,8},new int[] {8,4,6,2}),
-                arguments(new int[]{7,2,1,3},new int[] {1,7,3,2}),
-                arguments(new int[]{8,9,3,6},new int[] {9,3,6,8}),
+                arguments(new int[]{3,0,6,4},new int[] {0,3,4,6}),
+                arguments(new int[]{9,5,2,7},new int[] {2,5,7,9}),
+                arguments(new int[]{5,2,7,8},new int[] {2,5,7,8}),
+                arguments(new int[]{0,5,8,7},new int[] {0,5,7,8}),
                 arguments(new int[]{4,2,8,5},new int[] {2,4,5,8})
         );
     }
@@ -31,7 +31,7 @@ public class PriorityQueueTest {
         for (int i =0 ; i<random_array.length;i++){
             test.add(random_array[i]);
         }
-
+        //random_array add to priorityqueue   get result
         for(int i =0 ; i< random_array.length;i++){
             result[i] = test.poll();
 
@@ -39,7 +39,7 @@ public class PriorityQueueTest {
         assertArrayEquals(correct_array,result);
     }
 
-
+    //三個例外
     @Test
     public void InitialCapacityTest(){
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
